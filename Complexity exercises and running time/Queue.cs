b872965b8d7900e62edf83bd86e382b054b5c5a2
Michael Swisa <ms0504109182@gmail.com>
2024-09-04 12:10:09 +0300

    Adding a queue class with the functions - push, pop, peek, count

---
+++ Complexity exercises and running time/Queue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complexity_exercises_and_running_time
+{
+    internal class Queue { }
+}
---
+++ Complexity exercises and running time/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complexity_exercises_and_running_time
+{
+    internal class Stack
+    {
+        public Node Top { get; set; }
+        public int count { get; set; }
+
+        public void Push(int value)
+        {
+            Node newNode = new Node(value);
+            if (count == 0)
+            {
+                this.Top = newNode;
+            }
+            else
+            {
+                newNode.Next = this.Top;
+                this.Top = newNode;
+            }
+            count++;
+        }
+
+        public int Pop()
+        {
+            if (count == 0)
+                return -1;
+            int value = this.Top.Value;
+            this.Top = this.Top.Next;
+            count--;
+            return value;
+        }
+
+        public int Peek()
+        {
+            if (count == 0)
+                return -1;
+            return this.Top.Value;
+        }
+
+        public int Count()
+        {
+            return this.count;
+        }
+    }
+}
