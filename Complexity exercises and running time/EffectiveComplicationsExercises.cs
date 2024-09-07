ade95a01261744c6b6f09bedc5fe26dd5b1efe34
Michael Swisa <ms0504109182@gmail.com>
2024-09-07 23:30:31 +0300

    add Ex7MergeSortedArraysUnique to EffectiveComplicationsExercises

--- Complexity exercises and running time/EffectiveComplicationsExercises.cs
+++ Complexity exercises and running time/EffectiveComplicationsExercises.cs
@@ -2,6 +2,7 @@
 
 public class EffectiveComplicationsExercises
 {
+    // O(n) n * 8
     public static int Ex1CalculateOddSum(int[] arr)
     {
         int sumOddNumber = 0;
@@ -13,6 +14,7 @@ public class EffectiveComplicationsExercises
         return sumOddNumber;
     }
 
+    // O(n)
     public static int[] Ex2FindMaximumTwo(int[] arr)
     {
         int max1 = arr[0];
@@ -30,6 +32,7 @@ public class EffectiveComplicationsExercises
         return new int[] { max1, max2 };
     }
 
+    // O(n)
     public static bool Ex3ContainsNumber(int[] array, int number)
     {
         for (int i = 0; i < array.Length; i++)
@@ -40,6 +43,7 @@ public class EffectiveComplicationsExercises
         return false;
     }
 
+    // O(n)
     public static int[] Ex4SumOfAllTriplets(int[] array)
     {
         int num1 = array[0];
@@ -59,6 +63,7 @@ public class EffectiveComplicationsExercises
         return new int[] { num1, num2, num3 };
     }
 
+    // O(n)
     public static int[] Ex5FindLastClosePairWithProduct(int[] array, int product)
     {
         int num1 = int.MinValue;
@@ -74,8 +79,73 @@ public class EffectiveComplicationsExercises
         return new int[] { num1, num2 };
     }
 
-    //public static int[] Ex5FindLastNotClosePairWithProduct(int[] array, int product)
-    //{
+    // O(n)
+    public static int[] Ex5FindLastNotClosePairWithProduct(int[] array, int product)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            if (array[i] * array[i - 1] == product)
+            {
+                return new int[] { array[i], array[i - 1] };
+            }
+        }
+        return new int[] { 0, 0 };
+    }
 
-    //}
+    // O(n)
+    public static int Ex6CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+                count++;
+        }
+        return count;
+    }
+
+    //
+    public static int[] Ex7MergeSortedArraysUnique(int[] array1, int[] array2)
+    {
+        int[] newArray = new int[array1.Length + array2.Length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < array1.Length && j < array2.Length)
+        {
+            if (array1[i] < array2[j])
+            {
+                if (k == 0 || newArray[k - 1] != array1[i])
+                {
+                    newArray[k++] = array1[i];
+                }
+                i++;
+            }
+            else
+            {
+                if (k == 0 || newArray[k - 1] != array2[j])
+                {
+                    newArray[k++] = array2[j];
+                }
+                j++;
+            }
+        }
+        while (i < array1.Length)
+            if (newArray[k] != array1[i])
+                newArray[k++] = array1[i++];
+        while (j < array2.Length)
+            if (newArray[k] != array2[j])
+                newArray[k++] = array2[j++];
+        if (k < newArray.Length)
+        {
+            int[] tmp = new int[k];
+            for (int l = 0; l < k; l++)
+            {
+                tmp[l] = newArray[l];
+            }
+            newArray = tmp;
+        }
+
+        return newArray;
+    }
 }
