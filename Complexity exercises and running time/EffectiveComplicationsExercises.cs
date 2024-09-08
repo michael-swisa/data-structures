688a6f5da32f9ea6e0ff0223fc7243a5eed07825
Michael Swisa <ms0504109182@gmail.com>
2024-09-08 20:47:08 +0300

    add Ex8FindPairsWithSum, Ex9CountFrequency, Ex10FindMaxSubarraySum,  Ex11FindFirstRepeating,  Ex12FindMissingNumber, Ex13FindMissingNumber to EffectiveComplicationsExercises

--- Complexity exercises and running time/EffectiveComplicationsExercises.cs
+++ Complexity exercises and running time/EffectiveComplicationsExercises.cs
@@ -148,4 +148,98 @@ public class EffectiveComplicationsExercises
 
         return newArray;
     }
+
+    public static void Ex8FindPairsWithSum(int[] array, int sum)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = i + 1; j < array.Length; j++)
+            {
+                if (array[i] + array[j] == sum)
+                {
+                    Console.WriteLine($"{array[i]} + {array[j]} = {sum}");
+                }
+            }
+        }
+    }
+
+    public static int[] Ex9CountFrequency(int[] array)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            try
+            {
+                dict[array[i]]++;
+            }
+            catch
+            {
+                dict.Add(array[i], 1);
+            }
+        }
+        return dict.Values.ToArray();
+    }
+
+    // O(n)
+    public static int Ex10FindMaxSubarraySum(int[] array)
+    {
+        int maxSum = array[0];
+        int count = array[0];
+
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] == array[i - 1] + 1)
+            {
+                count += array[i];
+            }
+            else
+            {
+                maxSum = Math.Max(maxSum, count);
+                count = array[i];
+            }
+        }
+        maxSum = Math.Max(maxSum, count);
+        return maxSum;
+    }
+
+    public static int Ex11FindFirstRepeating(int[] array)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < array.Length; i++)
+        {
+            try
+            {
+                dict[array[i]]++;
+                return array[i];
+            }
+            catch
+            {
+                dict.Add(array[i], 1);
+            }
+        }
+        return -1;
+    }
+
+    public static int Ex12FindMissingNumber(int[] array, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] != i + 1)
+            {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int Ex13FindMissingNumber(int[] array, int n)
+    {
+        int sumOfRange = n * (n + 1) / 2;
+        int sumOfArray = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            sumOfArray += array[i];
+        }
+        return sumOfRange - sumOfArray;
+    }
 }
